4b8e96ffb1bdadbce301116a3139e00e9c1769c8
oscarvarto <oscarvarto@users.noreply.github.com>
2025-10-25 22:04:29 -0600

    Fixing JankGit class name, to follow homebrew expectations

--- Formula/jank-git.rb
+++ Formula/jank-git.rb
@@ -19,24 +19,36 @@ class JankGit < Formula
   def install
     ENV.prepend_path "PATH", Formula["llvm@21"].opt_bin
 
-    ENV.append "LDFLAGS", "-Wl,-rpath,#{Formula["llvm@21"].opt_lib}"
+    # Critical: Ensure libc++ headers come BEFORE SDK headers
+    # This fixes the macOS 26 header ordering issue (jank-lang/jank#560)
+    llvm_include = Formula["llvm@21"].opt_include
+    ENV.prepend "CPPFLAGS", "-isystem #{llvm_include}/c++/v1"
+    ENV.prepend "CXXFLAGS", "-isystem #{llvm_include}/c++/v1"
 
-    ENV.append "CPPFLAGS", "-L#{Formula["llvm@21"].opt_include}"
-    ENV.append "CPPFLAGS", "-fno-sized-deallocation"
-
+    # Use LLVM's clang explicitly on macOS to avoid Homebrew clang issues
     if OS.mac?
+      ENV["CC"] = Formula["llvm@21"].opt_bin/"clang"
+      ENV["CXX"] = Formula["llvm@21"].opt_bin/"clang++"
       ENV["SDKROOT"] = MacOS.sdk_path
     else
       ENV["CC"] = Formula["llvm@21"].opt_bin/"clang"
       ENV["CXX"] = Formula["llvm@21"].opt_bin/"clang++"
     end
 
+    ENV.append "LDFLAGS", "-Wl,-rpath,#{Formula["llvm@21"].opt_lib}"
+    ENV.append "LDFLAGS", "-L#{Formula["llvm@21"].opt_lib}"
+
+    ENV.append "CPPFLAGS", "-I#{llvm_include}"
+    ENV.append "CPPFLAGS", "-fno-sized-deallocation"
+
     cd "compiler+runtime"
 
     system "./bin/configure",
            "-GNinja",
            *std_cmake_args,
-           "-DHOMEBREW_ALLOW_FETCHCONTENT=ON"
+           "-DHOMEBREW_ALLOW_FETCHCONTENT=ON",
+           "-DCMAKE_CXX_COMPILER=#{Formula["llvm@21"].opt_bin}/clang++",
+           "-DCMAKE_C_COMPILER=#{Formula["llvm@21"].opt_bin}/clang"
     system "./bin/compile"
     system "./bin/install"
   end
