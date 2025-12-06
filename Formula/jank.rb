6410f4ff6073b5f6c26692b59241532c326db470
oscarvarto <oscarvarto@users.noreply.github.com>
2025-12-06 09:24:15 -0600

    Update jank binary formula with environment fixes
    Improve the binary distribution formula:
    - Add wrapper script that clears SDK/Nix environment variables
      to prevent JIT linker conflicts
    - Update tarball URL after jank PR 585 was merged
    - Add skip_clean directive to avoid JIT linker issues
    - Include health check test to verify AOT compilation works
    
    This provides a quick installation path without requiring LLVM HEAD
    or building from source.

--- Formula/jank.rb
+++ Formula/jank.rb
@@ -1,60 +1,46 @@
 class Jank < Formula
-  desc "Native Clojure dialect hosted on LLVM"
+  desc "The native Clojure dialect hosted on LLVM with seamless C++ interop."
   homepage "https://jank-lang.org"
-  url "https://github.com/jank-lang/jank.git", branch: "main"
+  url "https://cache.jank-lang.org/jank_0.1-1_aarch64.tar.gz"
   version "0.1"
   license "MPL-2.0"
 
-  depends_on "cmake" => :build
-  depends_on "git-lfs" => :build
-  depends_on "ninja" => :build
-
-  depends_on "bdw-gc"
   depends_on "boost"
   depends_on "libzip"
-  depends_on "llvm@21"
   depends_on "openssl"
 
+  skip_clean "bin/jank"
+
   def install
-    ENV.prepend_path "PATH", Formula["llvm@21"].opt_bin
-
-    ENV.append "LDFLAGS", "-Wl,-rpath,#{Formula["llvm@21"].opt_lib}"
-
-    ENV.append "CPPFLAGS", "-L#{Formula["llvm@21"].opt_include}"
-    ENV.append "CPPFLAGS", "-fno-sized-deallocation"
-
-    jank_install_dir = OS.linux? ? libexec : bin
-    inreplace "compiler+runtime/cmake/install.cmake",
-              '\\$ORIGIN',
-              jank_install_dir
-
-    if OS.mac?
-      ENV["SDKROOT"] = MacOS.sdk_path
-    else
-      ENV["CC"] = Formula["llvm@21"].opt_bin/"clang"
-      ENV["CXX"] = Formula["llvm@21"].opt_bin/"clang++"
+    (buildpath/"usr/local").cd do
+      cp_r Dir["*"], prefix
     end
 
-    cd "compiler+runtime"
-
-    system "./bin/configure",
-           "-GNinja",
-           *std_cmake_args,
-           "-DHOMEBREW_ALLOW_FETCHCONTENT=ON"
-    system "./bin/compile"
-    system "./bin/install"
+    libexec_bin = libexec/"bin"
+    libexec_bin.install bin/"jank"
+    (bin/"jank").write <<~SH
+      #!/usr/bin/env bash
+      set -euo pipefail
+      unset SDKROOT
+      unset HOMEBREW_SDKROOT
+      unset MACOSX_DEPLOYMENT_TARGET
+      unset NIX_CFLAGS_COMPILE
+      unset NIX_LDFLAGS
+      unset NIX_APPLE_SDK_VERSION
+      unset NIX_APPLE_SDK_ROOT
+      exec "#{libexec_bin/"jank"}" "$@"
+    SH
+    (bin/"jank").chmod 0755
+    ln_s lib, libexec/"lib", force: true
   end
 
   test do
     jank = bin/"jank"
 
-    (testpath/"test.jank").write <<~JANK
-      ((fn [] (+ 5 7)))
-    JANK
-
-    assert_equal "12", shell_output("#{jank} run test.jank").strip.lines.last
-
     assert_predicate jank, :exist?, "jank must exist"
     assert_predicate jank, :executable?, "jank must be executable"
+
+    health_check = pipe_output("#{jank} check-health")
+    assert_match "jank can aot compile working binaries", health_check
   end
 end
